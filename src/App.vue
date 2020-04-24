25a488950d9b96dfe713428d52acf9796917a464
Maxim Bredikhin <max11298@yandex.ru>
2020-04-24 16:14:15 +0300

    Update App styles

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,30 @@
   <div id="app"></div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+:root {
+  --blue: #2eaadc;
+  --red: #db253d;
+  --green: #36ca5b;
+  --merlin: #37352f;
+  --gray: #5d5c57;
+  --cloudy: #afaeac;
+  --light-gray: #b3b2b0;
+  --alto: #dcdcdc;
+  --cream: #f7f6f3;
+  --alabaster: #fafafa;
+  --main-font: 'Oxygen', sans-serif;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  padding: 5vh 5vw;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
